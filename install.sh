9b5ce3c474c18c0da7993763af7576a70a28f7eb
Ariel Marcovitch <arielmarcovitch@gmail.com>
2023-10-14 18:51:37 +0300

    install.sh: Update busybox in the image to a better one
    Use the one from `busybox-static` apt instead of the one from `busybox-initramfs`

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 sudo cp runmore /etc/initramfs-tools/scripts/local-premount/runmore
 sudo cp somemore /etc/initramfs-tools/hooks/somemore
 
+# Use a better busybox
+sudo cp /usr/share/initramfs-tools/hooks/zz-busybox-initramfs /etc/initramfs-tools/hooks/zzz-use-full-busybox 
+sudo sed -i 's|/usr/lib/initramfs-tools/bin/busybox|/bin/busybox|' /etc/initramfs-tools/hooks/zzz-use-full-busybox
+
 add="source $(dirname "$(realpath $0)")/qemu.sh"
 if ! grep -wq "$add" ~/.bashrc; then
     echo "$add" >> ~/.bashrc
